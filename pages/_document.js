0993e50eafe8c691760004d22e64a375a6dd7c8f
Rocky Li <rocky.li@scmp.com>
2020-10-26 03:02:38 +0800

    i3-configure-styled-components-ssr

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { getLangFromReq } from '../utils/fromReq'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
     const lang = getLangFromReq(ctx.req)
-    return { ...initialProps, lang }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () => 
+        originalRenderPage({
+          enhanceApp: App => props => 
+            sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        lang,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
