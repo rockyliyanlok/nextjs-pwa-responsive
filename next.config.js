f8ca89ef99f19803d025927d4fd5fad756cccfdd
Rocky Li <rocky.li@scmp.com>
2020-10-26 01:38:20 +0800

    i1-pull-nextjs-100-as-boilerplate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')
  
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public'
-//   }
-// })
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  }
+})
