838eb07c625f67da0f0549cf9452d125a45858a6
Yanlok Li <yli@bol.com>
2021-11-11 21:42:04 +0100

    i17-upgrade-the-dependencies

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { getCsp } from '../utils/csp'
 import { getLangFromReq } from '../utils/fromReq'
 import { ServerStyleSheet } from 'styled-components'
 
@@ -9,11 +10,9 @@ class MyDocument extends Document {
     const originalRenderPage = ctx.renderPage
 
     try {
-      ctx.renderPage = () => 
-        originalRenderPage({
-          enhanceApp: App => props => 
-            sheet.collectStyles(<App {...props} />)
-        })
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      })
 
       const initialProps = await Document.getInitialProps(ctx)
       return {
@@ -37,8 +36,11 @@ class MyDocument extends Document {
         <Head>
           <meta name="description" content="Build a responsive PWA using Next.js and styled-components."></meta>
           <meta name="theme-color" content="#FFFFFF"/>
+          <meta name="referrer" content={'strict-origin'} />
+          <meta httpEquiv="Content-Security-Policy" content={getCsp(NextScript.getInlineScriptSource(this.props))} />
           
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
           <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
