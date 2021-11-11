ff9ec92ae3c2923155634525f6b19afdf268f524
Yanlok Li <yli@bol.com>
2021-11-11 21:55:21 +0100

    i17-upgrade-the-dependencies

--- utils/csp.js
+++ utils/csp.js
@@ -5,12 +5,13 @@ const isProd = process.env.NODE_ENV === 'production'
 const getCsp = inlineScriptSource => {
   const csp = []
   const hash = crypto.createHash('sha256').update(inlineScriptSource)
+  const digest = hash.digest('base64')
 
   csp.push(`base-uri 'self'`)
   csp.push(`form-action 'self'`)
   csp.push(`default-src 'self'`)
-  csp.push(`script-src 'self'${isProd ? '' : ` 'unsafe-eval'`} 'sha256-${hash.digest('base64')}'`)
-  csp.push(`style-src 'self'${isProd ? '': ` 'unsafe-inline'`} 'sha256-${hash.digest('base64')}'`)
+  csp.push(`script-src 'self'${isProd ? '' : ` 'unsafe-eval'`} 'sha256-${digest}'`)
+  csp.push(`style-src 'self'${isProd ? '': ` 'unsafe-inline'`} 'sha256-${digest}'`)
   csp.push(`connect-src 'self' vitals.vercel-insights.com`)
   csp.push(`img-src 'self' data: blob:`)
   csp.push(`font-src 'self' data:`)
